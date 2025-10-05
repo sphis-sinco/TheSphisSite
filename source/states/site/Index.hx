76e13c5d68ad5893a3a45ea1d8e8ff843deddc5f
Viwu <sinconsistencia@gmail.com>
2025-10-05 09:07:03 -0500

    Index instance

--- source/states/site/Index.hx
+++ source/states/site/Index.hx
@@ -2,8 +2,13 @@ package states.site;
 
 class Index extends Page
 {
+	public static var instance:Index;
+
 	override public function new()
 	{
 		super('index');
+		if (instance != null)
+			instance = null;
+		instance = this;
 	}
 }
